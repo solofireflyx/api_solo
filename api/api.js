08b2e038c37f3e1488180d5c3cb48fd0e5ec75a2
solo.fireflyx <78276510+solofireflyx@users.noreply.github.com>
2022-05-13 15:08:22 +0000

    Initial commit

---
+++ api/api.js
@@ -0,0 +1,6 @@
+function myapi {
+  in Document();
+  /// Document is api/run.zxr
+  if data (api/run.zxr) => file();
+  /// run file data
+}
--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 const mySecret = process.env['config.json']
-const express = require('express');
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('hello wellcome')
-});
-
-app.listen(3000, () => {
-  console.log('server started');
-});
